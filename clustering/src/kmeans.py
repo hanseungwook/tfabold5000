4d6803ee6011dec29f896ae21fe3b774213fd31a
Daniel Jeong <daniel.jeong@columbia.edu>
2018-12-09 23:09:53 -0500

    Update kmeans for better output formatting and progress bar revision

--- clustering/src/kmeans.py
+++ clustering/src/kmeans.py
@@ -4,7 +4,8 @@ from sklearn import metrics
 import numpy as np
 import matplotlib.pyplot as plt
 import progressbar as pb
-from progressbar import ProgressBar, ETA, Bar
+from progressbar import ProgressBar, ETA, Bar, Timer
+# from collections import defaultdict
 import argparse
 import multiprocessing as mp
 import yaml
@@ -13,10 +14,11 @@ import yaml
 K_START = 100
 K_END = 500
 TRIAL_NUM = 5
+IMAGE_PATH = '../../bold5000-dataset/scene'
 FIGURE_PATH = '../figures'
 LABEL_PATH = '../labels'
 
-def find_best_k(features_list_np, model, show_plot=False):
+def find_best_k(images, features_list_np, show_plot=False):
     k_list = np.linspace(K_START, K_END, num=TRIAL_NUM, dtype=np.int32)
 
     silhouette_scores = []
@@ -24,7 +26,7 @@ def find_best_k(features_list_np, model, show_plot=False):
     print('{} cores found.'.format(mp.cpu_count()))
 
     trial = 1
-    progress = ProgressBar(widgets=['Trial {}'.format(trial), ' ', pb.Percentage(), ' ',  Bar('='), ' ', ETA()])
+    progress = ProgressBar(widgets=['Trial {}'.format(trial), ' ', pb.Percentage(), ' ',  Bar('='), ' ', Timer()]).start()
     for k in progress(k_list):
         print('Clustering for K = {}...'.format(k))
         trial += 1
@@ -33,11 +35,13 @@ def find_best_k(features_list_np, model, show_plot=False):
         # result = pool.map(kmeans.fit, features_list_np)
         result = KMeans(n_clusters=k, random_state=0, n_jobs=-1, verbose=0).fit(features_list_np)
         labels = result.labels_
+        image_label_pairs = list(zip(images, labels))
 
         if not os.path.isdir(LABEL_PATH):
             os.mkdir(LABEL_PATH)
-        labelfile = open(os.path.join(LABEL_PATH,'{}_labels_{}'.format(model, k)), 'w')
-        np.savetxt(labelfile, labels)
+        labelfile = open(os.path.join(LABEL_PATH,'labels_{}.yml'.format(k)), 'w')
+        #np.savetxt(labelfile, image_label_pairs)
+        yaml.dump(image_label_pairs, labelfile, default_flow_style=False)
         labelfile.close()
 
         # Silhouette score = [-1, 1]; -1 = incorrect clustering, 1 = highly dense clustering (well-separated)
@@ -51,14 +55,14 @@ def find_best_k(features_list_np, model, show_plot=False):
     ax_sil.set_xlabel('K')
     ax_sil.set_ylabel('Silhouette Score')
     ax_sil.set_title('Silhouette Score vs. K')
-    ax_sil.plot(k_list, silhouette_scores)
+    ax_sil.scatter(k_list, silhouette_scores)
 
     if not os.path.isdir(FIGURE_PATH):
         os.mkdir(FIGURE_PATH)
 
     # Save plot and Silhouette scores
-    plt.savefig(os.path.join(FIGURE_PATH, '{}_silhouette_vs_k.png'.format(model)))
-    outfile = open(os.path.join(FIGURE_PATH, '{}_results.txt'.format(model)), 'w+')
+    plt.savefig(os.path.join(FIGURE_PATH, 'silhouette_vs_k.png'))
+    outfile = open(os.path.join(FIGURE_PATH, 'silhouette_vs_k.txt'), 'w+')
     results = list(zip(k_list, silhouette_scores))
     #yaml.dump(results, outfile, default_flow_style=True)
     np.savetxt(outfile, results)
@@ -77,11 +81,13 @@ def find_best_k(features_list_np, model, show_plot=False):
     return best_k
 
 # Dimensions = (4916, 11, 11, 512)
+# Dominant color dimensions = (1000, 5, 3)
 def main(**kwargs):
-    filename = kwargs['features_file']
-    filepath = filename
-    idx = filename.find('total_features')
-    model = filename[idx+15:][:-4]
+    imagepath = kwargs['image_path']
+    filepath = kwargs['features_file']
+    #idx = filename.find('total_features')
+    #model = filename[idx+15:][:-4]
+    images = os.listdir(imagepath)
     features = np.load(filepath)
     features_list = []
     for i in range(features.shape[0]):
@@ -91,13 +97,14 @@ def main(**kwargs):
     print('Features loaded.')
     # print(features_list_np.shape)
 
-    best_k = find_best_k(features_list_np, model, kwargs['show_plot'])
+    best_k = find_best_k(images, features_list_np, kwargs['show_plot'])
 
     print('K_START: {}, K_END: {} -> Best K: {}'.format(K_START, K_END, best_k))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Takes in VGG-extracted features as input')
-    parser.add_argument('features_file', help='File containing VGG-extracted features')
+    parser.add_argument('image_path', help='Path to image data')
+    parser.add_argument('features_file', help='File containing features')
     parser.add_argument('--show_plot', help='Flag to enable showing plot', action='store_true', default=False)
     args = parser.parse_args()
     main(**vars(args))
