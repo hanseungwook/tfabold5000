49546106a0b0b2fab1cbf478eda627f1e9442bed
Seungwook Han <sh3264@columbia.edu>
2018-12-10 03:47:57 -0500

    Adding silhouette scores for k in color extractor

--- clustering/src/color_extractor.py
+++ clustering/src/color_extractor.py
@@ -1,4 +1,5 @@
 from sklearn.cluster import KMeans
+from sklearn import metrics
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
@@ -12,7 +13,7 @@ import argparse
 import math
 
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-sparse_shape = (8, 8, 8)
+sparse_shape = (4, 4, 4)
 
 class ColorExtractor(object):
     def __init__(self, k = 5, img_dir = None):
@@ -45,7 +46,7 @@ class ColorExtractor(object):
                 clt.fit(img)
                 
                 #self.img_colors.append(clt.cluster_centers_)
-                self.img_colors.append(create_sparse_mat_rep(clt.cluster_centers_))
+                self.img_colors.append(self.create_sparse_mat_rep(clt.cluster_centers_))
             else:
                 Z = img.reshape((-1,3))
                 Z = np.float32(Z)
@@ -62,6 +63,31 @@ class ColorExtractor(object):
             #print(clt.cluster_centers_)
             counter += 1
             print('Progress: %f' % (round(counter / n, 3) * 100))
+
+    def find_best_KMeans(self, start, end, by):
+        k_list = np.arange(start=start, stop=end, step=by)
+        best_s = np.empty((len(k_list),))
+        
+        outer_counter = 0
+        for k in k_list:
+            s_score = np.empty((len(self.img,)))
+            inner_counter = 0
+
+            for img in self.img:
+                clt = KMeans(n_clusters = self.k, n_jobs=-1)
+                clt.fit(img)
+                
+                s_score[inner_counter] = metrics.silhouette_score(img, clt.labels_, metric='euclidean')
+                inner_counter += 1
+            
+            best_s[outer_counter] = np.mean(s_score)
+            
+            outer_counter += 1
+
+            print('Progress: %f' % (round(outer_counter / len(k_list), 3) * 100))
+        
+        return k_list[np.argmax(best_s)]
+            
 
     def create_sparse_mat_rep(self, centers):
         sparse_mat = np.zeros(sparse_shape)
@@ -103,8 +129,9 @@ def main():
 
     ce = ColorExtractor(k = int(args.k), img_dir = args.img_dir)
     ce.load_images()
-    ce.do_KMeans(q=1)
-    ce.save(args.out_file)
+    ce.find_best_KMeans(1, 10, 1)
+    # ce.do_KMeans(q=1)
+    # ce.save(args.out_file)
     #hist = ce.centroid_histogram(clt)
     #bar = ce.plot_colors(hist,clt.cluster_centers_)
     #print(clt.cluster_centers_)
